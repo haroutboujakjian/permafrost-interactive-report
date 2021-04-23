5738a389806956030922041f38d67b7a3934eaed
squarerout <haroutb719@gmail.com>
2021-04-23 15:05:46 -0400

    added ability to click on/off the sensor depth lines from the graphic

--- src/components/SensorDepths.vue
+++ src/components/SensorDepths.vue
@@ -6,25 +6,28 @@
 
     <div class="sensorDepthsContainer">
       <div class="depthColumn">
-        <h3>BONA</h3>
-        <h3>DEJU</h3>
-        <h3>HEAL</h3>
+        <GrassSoil :inactive-sensors="inactiveSensorDepths"
+                   @click="addOrRemoveSensorFromInactive($event)">
+        </GrassSoil>
       </div>
 
       <div class="depthColumn" ref="sensorChartContainer">
+        <h4>BONA</h4>
         <LineChart :plot-data="sensor_depths_split.BONA" x_key="week"
-                   :width="width" :height="height - 10" :margin="margin" :colors="color"
-                   xaxis-label="soil CO2">
+                   :width="width" :height="height - 35" :margin="margin" :colors="color"
+                   xaxis-label="ppm CO2" :inactive-lines="inactiveSensorDepths">
         </LineChart>
 
+        <h4>DEJU</h4>
         <LineChart :plot-data="sensor_depths_split.DEJU" x_key="week"
-                   :width="width" :height="height - 10" :margin="margin" :colors="color"
-                   xaxis-label="soil CO2">
+                   :width="width" :height="height - 35" :margin="margin" :colors="color"
+                   xaxis-label="ppm CO2" :inactive-lines="inactiveSensorDepths">
         </LineChart>
 
+        <h4>HEAL</h4>
         <LineChart :plot-data="sensor_depths_split.HEAL" x_key="week"
-                   :width="width" :height="height - 10" :margin="margin" :colors="color"
-                   xaxis-label="soil CO2">
+                   :width="width" :height="height - 35" :margin="margin" :colors="color"
+                   xaxis-label="ppm CO2" :inactive-lines="inactiveSensorDepths">
         </LineChart>
       </div>
 
@@ -39,20 +42,23 @@
 import weekly_sensor_depth_data from "@/data/weekly_co2_sensors_depth_3_sites.json"
 import LineChart from "@/components/LineChart";
 import BaseLegend from "@/components/BaseLegend";
+import GrassSoil from "@/components/GrassSoil";
 
 export default {
   name: "SensorDepths",
   components: {
     BaseLegend,
-    LineChart
+    LineChart,
+    GrassSoil
   },
   data() {
     return {
       sensor_depths: weekly_sensor_depth_data,
-      margin: {top: 20, bottom: 20, left: 50, right: 5},
+      margin: {top: 15, bottom: 25, left: 50, right: 20},
       color: ["#FFC6C6", "#FF5F5F", "#900101"],
       width: null,
-      height: null
+      height: null,
+      inactiveSensorDepths: []
     }
   },
   computed: {
@@ -85,6 +91,14 @@ export default {
     resizeWidthAndHeight() {
       this.width = this.$refs.sensorChartContainer.clientWidth - 1
       this.height = (this.$refs.sensorChartContainer.clientHeight - 1) / 3
+    },
+    addOrRemoveSensorFromInactive(sensor_id) {
+      if (this.inactiveSensorDepths.includes(sensor_id)) {
+        this.inactiveSensorDepths = this.inactiveSensorDepths
+            .filter(sensor => sensor !== sensor_id)
+      } else {
+        this.inactiveSensorDepths.push(sensor_id)
+      }
     }
   }
 }
@@ -118,14 +132,14 @@ export default {
 }
 
 .depthColumn:first-child {
-  width: 70px;
+  width: 230px;
 }
 
 .depthColumn:last-child {
-  width: calc(100% - 70px);
+  width: calc(100% - 230px);
 }
 
-h3 {
+h4 {
   height: 33%;
   display: flex;
   align-items: center;
